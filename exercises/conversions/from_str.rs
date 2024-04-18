2b86a7e17543076ab42972bf2d84c079f1e4cc09
Xin Xiu <2313466338@qq.com>
2024-04-18 07:30:12 +0000

    push complete from_str

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -55,6 +54,19 @@ impl FromStr for Person {
         if s.is_empty() {
             return Err(ParsePersonError::Empty);
         }
+
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }   
+
+        let name = parts[0].to_string();
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        
+        let age = parts[1].parse::<usize>().map_err(ParsePersonError::ParseInt)?;
+        Ok(Person {name, age})
     }
 }
 
